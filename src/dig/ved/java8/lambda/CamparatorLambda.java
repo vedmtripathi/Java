842de500da7b41cf0076d928cbc251222a750958
Vedbhasker Tripathi <vedbhasker.t@hclaig.com>
2018-08-27 22:17:23 +0530

    Java 8 lambda with comparator

---
+++ src/dig/ved/java8/lambda/CamparatorLambda.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package dig.ved.java8.lambda;
+
+/**
+ * @author vedbhasker.t
+ *
+ */
+public class CamparatorLambda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
